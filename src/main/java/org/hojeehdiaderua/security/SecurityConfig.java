5a330b6fbaf94dc8dca5e96e1a3af9b7af5e73e3
Paulo Ortolan (ext.) <Paulo.Ortolan@ctw.bmwgroup.com>
2020-09-19 19:25:20 +0100

    Adding security to project

---
+++ src/main/java/org/hojeehdiaderua/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.hojeehdiaderua.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
